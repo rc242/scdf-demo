2aa976c9f76203d171f7202c40e34a72c447904e
Robert Conway <rconway@rconwayRMD6R.vmware.com>
2023-10-17 10:39:59 -0500

    Updated to resolve potential race case with gradle push

--- scripts/setupRepository.sh
+++ scripts/setupRepository.sh
@@ -16,34 +16,37 @@ curl -X PUT http://localhost:8081/service/rest/v1/security/users/admin/change-pa
 password=$newPassword
 
 repoStatusCode=`curl --write-out "%{http_code}\n" --silent --output /dev/null -X GET "http://localhost:8081/service/rest/v1/repositories/maven/hosted/example" -u admin:${password}`
-
 if [[ $repoStatusCode == "200" ]] ; then
   echo "Repo alreaady created"
-else  
-  curl -X post http:/localhost:8081/service/rest/v1/repositories/maven/hosted -d '
-  {
-    "name": "example",
-    "online": true,
-    "storage": {
-      "blobStoreName": "default",
-      "strictContentTypeValidation": true,
-      "writePolicy": "allow_once"
-    },
-    "cleanup": {
-      "policyNames": [
-        "string"
-      ]
-    },
-    "component": {
-      "proprietaryComponents": true
-    },
-    "maven": {
-      "versionPolicy": "MIXED",
-      "layoutPolicy": "STRICT",
-      "contentDisposition": "ATTACHMENT"
-    }
-  }' -H 'Content-Type: application/json' -u admin:${password}
-
+else
+  echo "Trying to create the example repo"
+  while [[ $repoStatusCode != "200" ]] ; do 
+    curl -X post http:/localhost:8081/service/rest/v1/repositories/maven/hosted -d '
+    {
+      "name": "example",
+      "online": true,
+      "storage": {
+        "blobStoreName": "default",
+        "strictContentTypeValidation": true,
+        "writePolicy": "allow_once"
+      },
+      "cleanup": {
+        "policyNames": [
+          "string"
+        ]
+      },
+      "component": {
+        "proprietaryComponents": true
+      },
+      "maven": {
+        "versionPolicy": "MIXED",
+        "layoutPolicy": "STRICT",
+        "contentDisposition": "ATTACHMENT"
+      }
+    }' -H 'Content-Type: application/json' -u admin:${password}
+    repoStatusCode=`curl --write-out "%{http_code}\n" --silent --output /dev/null -X GET "http://localhost:8081/service/rest/v1/repositories/maven/hosted/example" -u admin:${password}`
+  done
+  echo "Repo created"
 fi
 cd ${projDir}/demo
 ./gradlew clean publish
